fef946347822afd9f97bc347f4e4cb14c16afb98
sabil <sabil-elhouda.chahbi@cea.fr>
2023-08-02 15:09:49 +0200

    node consuming std_msgs::String instead of msg type

--- src/sabilsubscriber.cpp
+++ src/sabilsubscriber.cpp
@@ -1,27 +1,27 @@
 #include "ros/ros.h"
-#include "sabilsubscriber/person_data.h"
+//#include "sabilsubscriber/person_data.h"
+#include <std_msgs/String.h>
 
-/*
-void writeMsgToLog(const std_msgs::String::ConstPtr& msg){
-    ROS_INFO("Hi i'm subscriber");
-    ROS_INFO("I'm receiving: %s", msg->data.c_str());
 
+void writeMsgToLog(const std_msgs::String::ConstPtr& msg)
+{
+    ROS_INFO("Hearing:[%s] ", msg->data.c_str());
+    ros::Rate rate(1);
+    while (ros::ok())
+    {
+    ros::spinOnce();
+    rate.sleep();
+    }
 }
-*/
 
-void writeMsgToLog(const sabilsubscriber::person_data &person_data){
-    ROS_INFO("Name : %s", person_data.name.c_str());
-    ROS_INFO("Age : %i", person_data.age);
-    ROS_INFO("Fav color : %s", person_data.color.c_str());
-
-}
 
 int main (int args, char **argv) {
+
     ros::init(args, argv, "sabilsubscriber");
+
     ros::NodeHandle n;
 
-    ros::Subscriber topic_sub = n.subscribe("/sabilData",1000,writeMsgToLog);
-    ros::Rate loop_rate(1);
+    ros::Subscriber topic_sub = n.subscribe("/sabilData", 1 ,writeMsgToLog);
 
     ros::spin();
 
